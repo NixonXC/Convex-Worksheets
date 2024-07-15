02e122bb94f0c8794b1221a7412232b39904cfbd
Dev-Atharv <invasionyt19@gmail.com>
2024-07-15 20:07:07 +0530

    Add files via upload

--- conversion.py
+++ conversion.py
@@ -14,7 +14,7 @@ def sanitize_content(content):
     illegal_chars = "#*$&%"
     return "".join(c for c in content if c not in illegal_chars)
 
-def create_pdf(header, description, data, no_of_questions, grade, user_input, diff):
+def create_pdf(header, description, data, no_of_questions, grade, user_input, diff, subject):
     pdf = FPDF('P', 'mm', 'A4')
     pdf.add_page()
     pdf.set_margins(20, 30, 20)
@@ -31,12 +31,12 @@ def create_pdf(header, description, data, no_of_questions, grade, user_input, di
     pdf.set_font('DejaVu', '', 12)
     pdf.multi_cell(0, 10, description, 0, 'C')
     pdf.set_font('DejaVu', 'B', 12)
-    pdf.multi_cell(0, 10, f"Chapter: {user_input} | Total Questions: {no_of_questions} | Grade: {grade}", 0, 'C')
-    pdf.multi_cell(0, 10, f"Difficulty: {diff}", 0, 'C')
+    pdf.multi_cell(0, 10, f"Chapter: {user_input} | Total Questions: {no_of_questions}", 0, 'C')
+    pdf.multi_cell(0, 10, f"Difficulty: {diff} | Subject: {subject} | Grade: {grade}", 0, 'C')
     pdf.set_font('DejaVu', '', 12)
 
     # Calculate the width of the underscores line
-    underscore_line = "__________________________________________________________"
+    underscore_line = "______________________________________________________________________"
     underscore_width = pdf.get_string_width(underscore_line)
     page_width = pdf.w - 2 * pdf.l_margin
     x = (page_width - underscore_width) / 2
@@ -59,3 +59,54 @@ def create_pdf(header, description, data, no_of_questions, grade, user_input, di
     output_path = os.path.join(output_folder, filename)
     pdf.output(output_path)
     return filename
+
+def generate_fsheet(header, description, data, type, grade, user_input, subject):
+    pdf = FPDF('P', 'mm', 'A4')
+    pdf.add_page()
+    pdf.set_margins(20, 30, 20)
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    pdf.add_font('DejaVu', '', 'DejaVuSans.ttf', uni=True)
+    pdf.add_font('DejaVu', 'B', 'DejaVuSans-Bold.ttf', uni=True)
+
+    # Font for the header
+    pdf.set_font('DejaVu', 'B', 26)
+    pdf.set_y(10)
+    pdf.multi_cell(0, 10, header, 0, 'C')
+
+    # Font for the body text
+    pdf.set_font('DejaVu', '', 12)
+    pdf.multi_cell(0, 10, description, 0, 'C')
+    pdf.set_font('DejaVu', 'B', 12)
+    if type == "sheet":
+        type = "Formula Sheet"
+    else:
+        type = "E-Notes"
+    pdf.multi_cell(0, 10, f"Chapter: {user_input} | Type: {type}", 0, 'C')
+    pdf.multi_cell(0, 10, f"Subject: {subject} | Grade: {grade}", 0, 'C')
+    pdf.set_font('DejaVu', '', 12)
+
+    # Calculate the width of the underscores line
+    underscore_line = "______________________________________________________________________"
+    underscore_width = pdf.get_string_width(underscore_line)
+    page_width = pdf.w - 2 * pdf.l_margin
+    x = (page_width - underscore_width) / 2
+
+    pdf.set_x(pdf.l_margin + x)
+    pdf.cell(underscore_width, 10, underscore_line, 0, 1, 'C')
+
+    pdf.cell(200, 10, '', 0, 1, 'C')
+
+    # Sanitize the content
+    sanitized_data = sanitize_content(data)
+    pdf.multi_cell(0, 10, sanitized_data)
+
+    sanitized_user_input = sanitize_filename(user_input)
+    random_string = generate_random_string()
+    filename = f"{sanitized_user_input}_{random_string}.pdf"
+    output_folder = os.path.join(current_dir, 'output')
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    output_path = os.path.join(output_folder, filename)
+    pdf.output(output_path)
+    return filename
+
--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import flask
 from flask import Flask, request, render_template, redirect, url_for, send_file, after_this_request
 import google.generativeai as genai
 import conversion as conversion
+import requests
 import os
+from google.generativeai.types import HarmCategory, HarmBlockThreshold
+from bs4 import BeautifulSoup, Comment # type: ignore
 
 app = Flask(__name__)
 
@@ -14,18 +17,42 @@ generation_config = {
         "top_k": 64,
         "max_output_tokens": 8192,
 }
+generation_config_2 = {
+        "temperature": 1,
+        "top_p": 0.95,
+        "top_k": 64,
+}
 
 model = genai.GenerativeModel(
         model_name="gemini-1.5-flash",
+        generation_config=generation_config_2,
+
+)
+f_model = genai.GenerativeModel(
+        model_name="gemini-1.5-flash",
         generation_config=generation_config,
+
 )
 
-def get_data(grade, user_input, diff, no_of_questions, header):
+def get_data(grade, user_input, diff, no_of_questions, header, subject):
     response = model.generate_content([
-            f"input: Generate {str(no_of_questions)} Mathematical questions based on the chapter {user_input} of class {grade} ncert, the questions must be {diff} in difficulty. Use formatting compatible with the DejaVuSans font/latin script such as subscript and instead of latex formatting, use formatting which can be represented in the DejaVuSans font.",
+            f"input: Generate {str(no_of_questions)} {subject} questions based on the chapter {user_input} of class {grade} ncert, the questions must be {diff} in difficulty. Use formatting compatible with the DejaVuSans font/latin script such as subscript and instead of latex formatting, use formatting which can be represented in the DejaVuSans font.",
             "output:",
         ])
-    return response.text, user_input, grade, diff, no_of_questions, header
+    return response.text[1:], user_input, grade, diff, no_of_questions, header
+
+def get_fsheet(grade, user_input, type, header, subject):
+    if type == "sheet":
+        response = f_model.generate_content([
+            f"input: Generate a formula sheet(A document only containing identities and formulas) on subject {subject} based on the chapter {user_input} of class {grade} NCERT(CBSE). Use formatting compatible with the DejaVuSans font/latin script such as subscript and instead of latex formatting, use formatting which can be represented in the DejaVuSans font.",
+            "output:",
+        ])
+    else:
+        response = f_model.generate_content([
+            f"input: Generate E-Notes(Digital notes having detailed explanations on the topics within a chapter) on subject {subject} based on the chapter {user_input} of class {grade} NCERT(CBSE). Use formatting compatible with the DejaVuSans font/latin script such as subscript and instead of latex formatting, use formatting which can be represented in the DejaVuSans font.",
+            "output:",
+        ])
+    return response.text[1:], user_input, grade, type, subject, header
 
 @app.route('/')
 def home():
@@ -38,10 +65,12 @@ def generate():
     diff = request.form['difficulty']
     no_of_questions = request.form['num_questions']
     header = request.form['header']
-    response = get_data(grade, user_input, diff, no_of_questions, header)
+    subject = request.form['subject']
+    response = get_data(grade, user_input, diff, no_of_questions, header, subject)
     pdf_filename = conversion.create_pdf(
     header=header,
-    description="Artifically Generated worksheets and sample papers for mathematics.",
+    subject=subject,
+    description="Artifically Generated worksheets and sample papers for CBSE.",
     data=response[0],
     user_input=response[1],
     grade=response[2],
@@ -49,6 +78,159 @@ def generate():
     no_of_questions=response[4]
     )
     return send_file(f"output/{pdf_filename}", as_attachment=True)
+
+@app.route('/books')
+def pdf():
+    return render_template('pdf.html')
+
+base_url = r'https://annas-archive.org'
+
+class Annas_Archive_Parser():
+    def __init__(self, params):
+        self.params = params
+
+    def get_top_five_links(self):
+        book_name = self.params["book-name"]
+        url = f"https://annas-archive.org/search?index=&q={book_name}"
+        response = requests.get(url=url)
+        data = response.text
+
+        soup = BeautifulSoup(data, 'html.parser')
+
+        # Find the container of each result
+        result_containers = soup.find_all("div", class_="h-[125] flex flex-col justify-center")
+
+        top_five_links = []
+        for result in result_containers[:5]:
+            link = result.find("a")
+            top_five_links.append(link)
+
+        links_1 = []
+        for link in top_five_links:
+            href_value = link.get('href')
+            links_1.append(href_value)
+
+        commented_a_tag = soup.find_all(string=lambda text: isinstance(text, Comment) and 'class="js-vim-focus custom-a' in text)
+
+        links_2 = []
+        for a_tag in commented_a_tag:
+            a_soup = BeautifulSoup(a_tag, 'html.parser')
+            href_value = a_soup.find('a')['href']
+            links_2.append(href_value)
+
+        links_1.extend(links_2)
+
+        return links_1
+    
+    def url_returns(self): 
+        links = self.get_top_five_links()
+        data = links[:5]
+        final_links = []
+        for item in data: 
+            final_url = base_url + item
+            final_links.append({
+                'id': final_url.replace("https://annas-archive.org/md5/",""),
+                'url': final_url,
+            })
+        return final_links
+
+def fetch_pdf_links_google(query):
+    base_url = f"https://www.google.com/search?q={query}&num=3&as_filetype=pdf"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+
+    response = requests.get(base_url, headers=headers)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        pdf_links = [a['href'] for a in soup.find_all('a', href=True) if '.pdf' in a['href']][:5]
+        return pdf_links
+    else:
+        print(f"Error: Unable to fetch Google search results. Status code: {response.status}")
+        return ["/"]
+
+def search_archive_org(book_title):
+    base_url = "https://archive.org/advancedsearch.php"
+    params = {
+        'q': f'title:"{book_title}" AND mediatype:texts',
+        'fl[]': 'identifier,title,creator',
+        'output': 'json'
+    }
+
+    response = requests.get(base_url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        docs = data.get('response', {}).get('docs', [])
+
+        results = []
+        for doc in docs[:3]:  # Get up to three results
+            identifier = doc.get('identifier', '')
+            title = doc.get('title', '')
+            creator = doc.get('creator', '')
+            url = f"https://archive.org/details/{identifier}"
+
+            results.append({
+                'identifier': identifier,
+                'title': title,
+                'creator': creator,
+                'url': url
+            })
+
+        return results
+    else:
+        return ["/"]
+
+@app.route('/methodcore', methods=["GET"])
+def formulasheets():
+    return render_template('fsheet.html')
+
+@app.route('/gen_sheet', methods=["POST"])
+def generate_fsheet():
+    grade = request.form['grade']
+    user_input = request.form['chapter']
+    type = request.form['type']
+    header = request.form['header']
+    subject = request.form['subject']
+    response = get_fsheet(grade, user_input, type, header, subject)
+    print(response)
+    pdf_filename = conversion.generate_fsheet(
+        header=header,
+        subject=subject,
+        description="Artifically Generated formula sheets and e-notes for CBSE.",
+        data=response[0],
+        user_input=response[1],
+        grade=response[2],
+        type=response[3]
+    )
+    return send_file(f"output/{pdf_filename}", as_attachment=True)
+
+@app.route('/search', methods=["POST"])
+def search():
+    pdf_name = request.form.get("pdf_name")
+    engine = request.form.get("engine")
+    
+    results = []
+
+    if engine == "google":
+        pdf_links = fetch_pdf_links_google(query=pdf_name)
+        for pdf_link in pdf_links:
+            filename = os.path.basename(pdf_link)
+            # Append each result to the results list
+            results.append({'pdf_name': filename, 'pdf_url': pdf_link})
+    elif engine == "archive":
+        archive_results = search_archive_org(book_title=pdf_name)
+        results = [{'pdf_name': result['title'], 'pdf_url': result['url']} for result in archive_results]
+    elif engine == "anna":
+        initialise = Annas_Archive_Parser(params={"book-name": "Art of war"})
+        final_links = initialise.url_returns()
+        results = [{'pdf_name': f"ID: {links['id']}", 'pdf_url': links['url']} for links in final_links]
+    else:
+        return redirect("/")
+
+    return render_template('results.html', results=results)
+
 
 if __name__ == '__main__':
         app.run(host='0.0.0.0', port=8080, debug=True)
